c24fbedea1e4d4b192c72aa83800193018b71ae6
Letícia Aguiar <leticia-aguiar@asus-de-leticia>
2023-07-01 13:44:55 -0300

    feat: create documentBuilder.setLogo()

--- source/document-builder.ts
+++ source/document-builder.ts
@@ -1,3 +1,10 @@
 import { DocumentBuilder as DefaultDocumentBuilder } from "@nestjs/swagger";
 
-export class DocumentBuilder extends DefaultDocumentBuilder { }
+export class DocumentBuilder extends DefaultDocumentBuilder {
+
+  public setLogo(url: string, altText?: string): this {
+    this['document']['x-logo'] = { url, altText };
+    return this;
+  }
+
+}
