f60078a013134656e2a409da4b1ad44873bd6eb6
Letícia Aguiar <leticia-aguiar@asus-de-leticia>
2023-07-01 13:44:31 -0300

    feat: update OpenAPIObject to include x-logo property

--- source/schemas/open-api.ts
+++ source/schemas/open-api.ts
@@ -1,3 +1,32 @@
 import { OpenAPIObject as DefaultOpenAPIObject } from "@nestjs/swagger";
 
-export type OpenAPIObject = DefaultOpenAPIObject;
+export interface OpenAPIObject extends DefaultOpenAPIObject {
+  info: InfoObject;
+}
+
+export interface InfoObject {
+  ["x-logo"]: LogoObject;
+
+  title: string;
+  description?: string;
+  termsOfService?: string;
+  contact?: ContactObject;
+  license?: LicenseObject;
+  version: string;
+}
+
+export interface ContactObject {
+  name?: string;
+  url?: string;
+  email?: string;
+}
+
+export interface LicenseObject {
+  name: string;
+  url?: string;
+}
+
+export interface LogoObject {
+  url: string;
+  altText: string;
+}
